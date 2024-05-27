5c1bc3c3f6c0880246665ac60da38b49368a135b
Irina NAHOANIKO <irina.nahoaniko@gmail.com>
2024-05-27 15:23:27 +0200

    block abc

---
+++ src/app/abc/abc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abc',
+  standalone: true,
+  imports: [],
+  templateUrl: './abc.component.html',
+  styleUrl: './abc.component.css'
+})
+export class AbcComponent {
+   title='ABC';
+}
